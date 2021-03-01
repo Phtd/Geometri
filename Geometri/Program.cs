d8fb825c3c33ac4eba08a11b058cba6d311ff469
Phtd <69340136+Phtd@users.noreply.github.com>
2021-03-01 05:59:29 +0100

    Add project files.

---
+++ Geometri/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Geometri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Square squareOne = new Square(20, 10, 15); // Sets the values of a, b and c. the sides of the square
+
+            Square squareTwo = new Square(20, 10, 15);
+
+            squareTwo.A = 25; // changes the value for A, by setting the value
+            squareTwo.B = 5;
+            squareTwo.C = 12;
+
+            Console.WriteLine("The circumference of Square 1 is = " + squareOne.Omkreds()); // Calls the method "omkreds" and uses it. Then it displays the result on the screen
+
+            Console.WriteLine("Area of Square 1 is = " + squareOne.Areal()); // Calls the method called "Areal" and uses it. Then it displays the result on the screen
+
+
+            Console.WriteLine("The circumference of Square 2 is = " + squareTwo.Omkreds());
+            Console.WriteLine("Area of Square  2 is = " + squareTwo.Areal());
+
+
+
+            Console.ReadKey(); // Prevents the program from shutting down once its done, unless a button is pressed
+        }
+    }
+}
---
+++ Geometri/Square.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Geometri
+{
+    public class Square
+    {
+        private float a; // My variable floats for the square sides
+        private float b;
+        private float c;
+        private float d;
+        public float A  // gets and sets the value of the float a
+        {
+            get { return a; }
+            set { a = value; }
+        }
+        public float B  // gets and sets the value of the float a
+        {
+            get { return b; }
+            set { b = value; }
+        }
+        public float C  // gets and sets the value of the float a
+        {
+            get { return c; }
+            set { c = value; }
+        }
+
+        public Square(float a, float b, float c) // the constructor and object called Square
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+
+
+        // A method called "Omkreds". It returns the circumference of a square, With the help of the variable float a.
+
+        public float Omkreds()
+        {
+            return A * 4; // Using this calculation, it returns the value of "Omkreds" 
+        }
+
+        //  A method called "Areal". 
+
+        public float Areal()
+        {
+            return B * C; // Using this calculation, it returns the value of the "Area" method
+        }
+
+        public Square(float a)
+        {
+            A = a;
+        }
+
+    }
+}
